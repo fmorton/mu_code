9a87cf1226ebd3e8988ef924d157185fd2f5d9cb
Frank Morton <fmorton@mac.com>
2024-12-21 13:39:47 -0500

    add xylophone.py

---
+++ xylophone.py
@@ -0,0 +1,80 @@
+from library import *
+
+TRAVEL_TIME = 0.25
+WHOLE_NOTE_DELAY = 0.5
+
+
+def reset():
+    bird.setPositionServo(2, 120)
+    bird.setPositionServo(1, 75)
+
+
+def play_note():
+    bird.setPositionServo(2, 96)
+    sleep(0.12)
+
+    bird.setPositionServo(2, 120)
+
+
+def note_angle(note):
+    if note == "C":
+        return 170  # lower C
+    if note == "D":
+        return 145
+    if note == "E":
+        return 115
+    if note == "F":
+        return 85
+    if note == "G":
+        return 55
+    if note == "A":
+        return 35
+    if note == "B":
+        return 15
+    if note == "2":
+        return 2
+
+    return 90
+
+
+def rhythym_delay(delay):
+    if delay == " ":
+        return WHOLE_NOTE_DELAY
+    if delay == "Q":
+        return WHOLE_NOTE_DELAY / 4
+    if delay == "H":
+        return WHOLE_NOTE_DELAY / 2
+    if delay == "W":
+        return WHOLE_NOTE_DELAY
+    if delay == "E":
+        return WHOLE_NOTE_DELAY / 8
+
+    return WHOLE_NOTE_DELAY
+
+
+def play(note, delay):
+    print(offset, note, delay, note_angle(note), rhythym_delay(delay))
+
+    if note != "-":
+        bird.setPositionServo(1, note_angle(note))
+        sleep(TRAVEL_TIME)
+
+        play_note()
+
+    sleep(rhythym_delay(delay))
+
+
+bird = Hummingbird("A")
+
+song = "CCDCFECCDCGFCCBAFEDBBAFGF"
+rhythym = "E    WE    WE     WE     "
+
+reset()
+
+for offset in range(len(song)):
+    note = song[offset]
+    delay = rhythym[offset]
+
+    play(song[offset], rhythym[offset])
+
+reset()
