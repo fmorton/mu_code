ae0acec7069b7731c8e6ee1c327241f10e5be03f
Frank Morton <fmorton@mac.com>
2025-01-13 15:54:50 -0500

    add template

---
+++ template.py
@@ -0,0 +1,31 @@
+import sys
+import asyncio
+import datetime
+import random
+
+from BirdBrain import Hummingbird
+from time import sleep
+
+bird = Hummingbird('A')
+
+LOOPS
+while True:
+while (i < 8):
+for index in range(0, 20):
+
+list = [ 'first', 'second', 'third' ]
+for s in list:
+
+for each_letter in 'a string':
+
+CONDITIONALS
+if a > 32:
+    pass
+elif a > 64:
+    pass
+else:
+    pass
+
+print("big") if a > 32 else print("little")
+
+if a > 32: print("big")
