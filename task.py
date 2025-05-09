58891df36dde5abe49a4d85b001bdf506f487d76
Frank Morton <fmorton@mac.com>
2025-05-09 09:42:59 -0400

    sync

--- task.py
+++ task.py
@@ -3,11 +3,14 @@ import asyncio
 
 from time import sleep
 
+#TODO: add create_subprocess_exec support
 
 class BirdbrainTasks:
     def __init__(self):
         self.method_list = []
         self.task_list = []
+        self.results = {}
+
 
     def create_task(self, method):
         self.method_list.append(method)
@@ -23,35 +26,49 @@ class BirdbrainTasks:
             running_task_count = 0
 
             for task in self.task_list:
+                try:
+                    self.results[task.get_coro().__name__] = task.result()
+                except asyncio.exceptions.InvalidStateError:
+                    pass
+
                 if not task.done(): running_task_count += 1
 
             if running_task_count == 0: break
 
-            await asyncio.sleep(0.1)
+            await self.yield_task()
+
+    @classmethod
+    async def yield_task(self, yield_time = 0.0):
+        await asyncio.sleep(yield_time)
 
 
 async def method_1(p):
-    for i in range(10):
+    for i in range(40):
         print("method_1 running", p, i)
 
-        await asyncio.sleep(0.1)
+        await BirdbrainTasks.yield_task()
+
+    return("method_1_return")
 
 async def method_2(p):
     for i in range(20):
         print("method_2 running", p, i)
 
-        await asyncio.sleep(0.1)
+        await BirdbrainTasks.yield_task()
 
 async def method_3():
     for i in range(30):
         print("method_3 running", i)
 
-        await asyncio.sleep(0.1)
+        await BirdbrainTasks.yield_task()
 
 tasks = BirdbrainTasks()
 
 tasks.create_task(method_1(999))
-tasks.create_task(method_2("parameter value"))
+tasks.create_task(method_2("text"))
 tasks.create_task(method_3())
 
 tasks.run()
+
+print("final results", tasks.results)
+
