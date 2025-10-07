14f6126e8733651fd8e8211aa3247dd569cc53f2
Frank Morton <fmorton@mac.com>
2025-10-07 17:28:03 -0400

    sync

---
+++ balance_robot.py
@@ -0,0 +1,49 @@
+from BirdBrain import Hummingbird
+from time import sleep
+import time
+
+MULTIPLIER = 1.0
+
+bird = Hummingbird("A")
+bird.stop_all()
+
+for i in range(10):
+    balanced = bird.magnetometer()[0]
+    print("balancing", balanced)
+    sleep(0.5)
+
+print("DEBUG: balanced is", balanced)
+def forward(bird, orientation):
+    #print("forward", orientation)
+    bird.rotation_servo(1, -11 * MULTIPLIER)
+    bird.rotation_servo(2, 25 * MULTIPLIER)
+    #sleep(0.1)
+    #stop(bird)
+
+def backward(bird, orientation):
+    #print("backward", orientation)
+    bird.rotation_servo(1, 10 * MULTIPLIER)
+    bird.rotation_servo(2, -11 * MULTIPLIER)
+    #sleep(0.1)
+    #stop(bird)
+
+def stop(bird):
+    #print("stop")
+    bird.rotation_servo(1, 0)
+    bird.rotation_servo(2, 0)
+
+orientation = balanced
+
+while True:
+    if orientation > balanced:
+        forward(bird, orientation)
+    elif orientation < balanced:
+        backward(bird, orientation)
+    else:
+        stop(bird)
+
+    start_time = time.perf_counter()
+    orientation = bird.magnetometer()[0]
+    end_time = time.perf_counter()
+    elapsed_time = end_time - start_time
+    print(f"The command took {elapsed_time:.4f} seconds to execute.")
---
+++ laser_target.py
@@ -0,0 +1,42 @@
+from BirdBrain import Hummingbird
+from time import sleep
+import random
+
+TARGET_SERVO = 1
+TARGET_HOME = 36
+TARGET_DOWN = 120
+
+MOVE_SERVO = 2
+MOVE_HOME = 165
+MOVE_LEFT_POSITION = 178
+MOVE_RIGHT_POSITION = 130
+
+HOME_NOT_HIT = 45
+HOME_HIT = 140
+
+bird = Hummingbird("A")
+
+bird.position_servo(TARGET_SERVO, TARGET_HOME)
+bird.position_servo(MOVE_SERVO, MOVE_HOME)
+
+move_count = 0
+move_target = random.randint(50, 150)
+
+while True:
+    bird.position_servo(TARGET_SERVO, TARGET_HOME)
+    move_target = random.randint(20, 40)
+
+    if(bird.light(1) < 92):
+        move_count += 1
+        if move_count > move_target:
+            move_count = 0
+            move_target = random.randint(MOVE_RIGHT_POSITION, MOVE_LEFT_POSITION)
+
+            bird.position_servo(MOVE_SERVO, move_target)
+    else:
+        bird.position_servo(TARGET_SERVO, TARGET_DOWN)
+        bird.position_servo(MOVE_SERVO, MOVE_HOME)
+
+        sleep(2)
+
+        bird.position_servo(TARGET_SERVO, TARGET_HOME)
